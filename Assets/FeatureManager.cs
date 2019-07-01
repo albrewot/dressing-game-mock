1887a73f4a8b961680f4bf870873b7958cc6d591
Albert Torres <albrewot@gmail.com>
2019-07-01 19:47:14 -0400

    [Mock up dressing game first commit]

---
+++ Assets/FeatureManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FeatureManager : MonoBehaviour
+{
+
+    public List<Feature> features;
+    public int currentFeature;
+
+
+    void OnEnable()
+    {
+        LoadFeatures();
+    }
+
+    void OnDisable()
+    {
+        SaveFeatures();
+    }
+
+    void LoadFeatures()
+    {
+        features = new List<Feature>();
+        features.Add(new Feature("Face", transform.Find("Face").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Hair", transform.Find("Face/Hair").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Eyes", transform.Find("Face/Eyes").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Mouth", transform.Find("Face/Mouth").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Tops", transform.Find("Top").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Bottoms", transform.Find("Bottom").GetComponent<SpriteRenderer>()));
+
+        for(int i = 0; features.Count > i; i++)
+        {
+            string key = "FEATURE_" + i;
+            if (!PlayerPrefs.HasKey(key))
+                PlayerPrefs.SetInt(key, features[i].currentIndex);
+            features[i].currentIndex = PlayerPrefs.GetInt(key);
+            features[i].UpdateFeature();
+        }
+    }
+
+    void SaveFeatures()
+    {
+        for(int i = 0; features.Count > i; i++)
+        {
+            string key = "FEATURE_" + i;
+            PlayerPrefs.SetInt(key, features[i].currentIndex);
+        }
+        PlayerPrefs.Save();
+    }
+
+    public void SetCurrent(int index)
+    {
+        if (features == null)
+            return;
+        currentFeature = index;
+    }
+
+    public void NextChoice()
+    {
+        if (features == null)
+            return;
+        features[currentFeature].currentIndex++;
+        features[currentFeature].UpdateFeature();
+    }
+
+    public void PrevChoice()
+    {
+        if (features == null)
+            return;
+        features[currentFeature].currentIndex--;
+        features[currentFeature].UpdateFeature();
+    }
+
+}
+
+[System.Serializable]
+
+public class Feature
+{
+    public string ID;
+    public int currentIndex;
+    public Sprite[] choices;
+    public SpriteRenderer renderer;
+
+    public Feature(string id, SpriteRenderer rend)
+    {
+        ID = id;
+        renderer = rend;
+        UpdateFeature();
+    }
+
+    public void UpdateFeature()
+    {
+        choices = Resources.LoadAll<Sprite>("Textures/" + ID);
+        if (choices == null || renderer == null)
+            return;
+        if (currentIndex < 0)
+            currentIndex = choices.Length - 1;
+        if (currentIndex >= choices.Length)
+            currentIndex = 0;
+        renderer.sprite = choices[currentIndex];
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform btnPrefab;
+    private FeatureManager featureManager;
+    private Text descriptionText;
+    private List<Button> buttons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        featureManager = FindObjectOfType<FeatureManager>();
+        descriptionText = transform.Find("Navigation/Selected").GetComponent<Text>();
+        transform.Find("Navigation/Previous").GetComponent<Button>().onClick.AddListener(() => featureManager.PrevChoice());
+        transform.Find("Navigation/Next").GetComponent<Button>().onClick.AddListener(() => featureManager.NextChoice());
+        InitializeFeatureButtons();
+    }
+
+    void InitializeFeatureButtons()
+    {
+        buttons = new List<Button>();
+
+        float height = btnPrefab.rect.height;
+        float width = btnPrefab.rect.width;
+
+        for (int i = 0; featureManager.features.Count > i; i++)
+        {
+            RectTransform temp = Instantiate<RectTransform>(btnPrefab);
+            temp.name = i.ToString();
+            temp.SetParent(transform.Find("Features").GetComponent<RectTransform>());
+            temp.localScale = new Vector3(1, 1, 1);
+            temp.localPosition = new Vector3(0, 0, 0);
+            temp.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, i > 3 ? 1 * width : 0 , width);
+            temp.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, i > 3 ? (i-4) * height : i * height, height);
+
+            Button b = temp.GetComponent<Button>();
+            b.onClick.AddListener(() => featureManager.SetCurrent(int.Parse(temp.name)));
+            buttons.Add(b);
+        }
+    }
+    void UpdateFeatureButtons()
+    {
+        for(int i = 0; featureManager.features.Count > i; i++)
+        {
+            buttons[i].transform.Find("FeatureImg").GetComponent<Image>().sprite = featureManager.features[i].renderer.sprite;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateFeatureButtons();
+        EventSystem.current.SetSelectedGameObject(buttons[featureManager.currentFeature].gameObject);
+        descriptionText.text = featureManager.features[featureManager.currentFeature].ID + " #" + (featureManager.features[featureManager.currentFeature].currentIndex + 1).ToString();
+    }
+}
